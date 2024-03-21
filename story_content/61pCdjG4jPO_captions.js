db22fe0f3f6bb6a7dfa6a95e4e673682123ed9d3
Mike Stein Design <>
2024-03-21 11:20:55 -0300

    v1

---
+++ story_content/61pCdjG4jPO_captions.js
@@ -0,0 +1,1 @@
+window.globalProvideData('caption', '{"data":"WEBVTT%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%206.80.31058.0%0D%0A%0D%0A00:00:00.096%20--%3E%2000:00:04.884%0D%0ASafety%20Steve%20needs%20to%20wear%20closed-toe%20work%20boots%20to%20protect%20his%20feet.%20%0D%0A%0D%0A"}')
---
+++ story_content/6g1clGSCU9Y_captions.js
@@ -0,0 +1,1 @@
+window.globalProvideData('caption', '{"data":"WEBVTT%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%206.80.31058.0%0D%0A%0D%0A00:00:00.096%20--%3E%2000:00:04.930%0D%0ASafety%20Steve%20needs%20to%20wear%20long%20pants%20to%20protect%20his%20legs%0D%0A%0D%0A"}')
